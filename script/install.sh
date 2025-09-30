c439dcd8613646a3fa7561c80615361f1d69b09f
RCMAN <franco.gaetan@gmail.com>
2025-09-30 10:49:48 -0400

    Create install.sh

---
+++ script/install.sh
@@ -0,0 +1,18 @@
+MOPD install
+
+sudo apt install git build-essential tftpd bootp nfs-kernel-server
+
+Determine the ethernet adapter name
+
+Edit the file /etc/network/interfaces
+
+# The primary network interface
+# allow-hotplug enp3s0
+auto enp3s0
+iface enp3s0 inet static
+        address 10.0.0.1/24
+		up /usr/bin/ip link set enp3s0 promisc on
+		
+		
+		git clone https://github.com/dreamlayers/netbsd-mopd.git
+		
